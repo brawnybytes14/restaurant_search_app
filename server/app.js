aa4f80e1fa125fd21483f989c51239b227906b34
Rohit Katariya <rohitkori00@gmail.com>
2023-06-23 00:10:56 +0530

    update test

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000 }));
 app.use(bodyParser.json({ limit: "50mb" }));
 
+
 //apis
 app.use(route);
 //end of apis
